84ff1cb086f5b20907657a7e6fa0f45a8b0a9787
MukeshMhanta02 <108122351+MukeshMhanta02@users.noreply.github.com>
2023-11-03 17:13:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.listen(3000, () => {
     console.log("Server Listening on PORT:", 3000);
 });
 
-app.get('/status', async (request, response) => {
+app.get('/', async (request, response) => {
     let result;
     const uri = 'mongodb+srv://mukesh:dell%40123@cluster0.tnohwgw.mongodb.net/'
     //const uri = 'mongodb://localhost:27017';
